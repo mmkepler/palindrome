ed58de7a76e7866a8798659834960a911d9165f8
M. Kepler <mmkepler@yahoo.com>
2022-07-07 11:18:48 -0600

    fix: remove spaces from string

--- src/App.js
+++ src/App.js
@@ -5,37 +5,44 @@ function App() {
   const InitialState = {input: ""};
 
   const [state, setState] = useState(InitialState);
+  //palindrome fails
   const [failure, setFailure] = useState(false);
+  //palindrome passes
   const [success, setSuccess] = useState(false);
 
+  //handle input change
   const inputHandler = (e) => {
     e.preventDefault();
-    console.log(e.target.value);
     setState({...state, input: e.target.value})
   };
 
+  //check input short function
   const palindromeCheck = (str) => {
-    let reverseString = str.split("").reverse().join("");
-    if(str === reverseString){
+    //remove spaces
+    let normalString = str.toLowerCase().replace(/\s/g, '');
+    //could have used a for loop for this, chose the shorter code
+    let reverseString = normalString.split("").reverse().join("");
+    console.log(normalString, reverseString);
+    if(normalString === reverseString){
       setSuccess(true);
     } else {
       setFailure(true);
     }
   }
-
+ 
+  //clear input on click
   const clearInput = () => {
     setState({...state, input: ""});
     setFailure(false);
     setSuccess(false);
   }
 
-
   return (
     <Main>
       <Title>Palindrome Checker</Title>
       
-      {failure  ? <><h2>It's not a match</h2><br></br><Clear onClick={() => clearInput()}>Try Again</Clear></> : ""}
-      {success ? <><h2>It's a match</h2><br></br><Clear onClick={() => clearInput()}>Try Again</Clear></> : ""}
+      {failure  ? <><h2>Not a Match!</h2><br></br><Clear onClick={() => clearInput()}>Try Again</Clear></> : ""}
+      {success ? <><h2>Match!</h2><br></br><Clear onClick={() => clearInput()}>Try Again</Clear></> : ""}
       { (failure === true || success === true) ?  "" : 
       (<InputDiv>
         
@@ -47,6 +54,7 @@ function App() {
           onChange={(e) => inputHandler(e)} 
           id="input"
           placeholder="Enter a string"
+          required
           />
        
         <br></br>
